929a3da4400c8a1b6b340d96868d17980f8fe47e
sebas2287ti <sebas2287ti@gmail.com>
2026-08-12 22:32:34 -0500

    remain: if to a match

--- src/algoritmos_ordenamiento/infrastructure/interface/main_window.py
+++ src/algoritmos_ordenamiento/infrastructure/interface/main_window.py
@@ -3,6 +3,7 @@ from algoritmos_ordenamiento.infrastructure.data.array_generator import generato
 import os
 
 def interface():
+    os.system("cls")
     print("!Bievenido a verificador de algoritmos!")
     input("presiona enter para continuar")
     os.system("cls")
@@ -13,18 +14,19 @@ def interface():
             print("Que deseas hacer")
             print("1 | Para crear un array personalizado con funcion aleatorio")
             print("2 | Para observar el tiempo de los algoritmos")
-            option = input("Ingresa la opcion: ")
-            if(option != "1" and option != "2"):
-                raise ValueError("")
-            elif(option == "1"):
-                generator_array()
-            else:
-                access_algoritms()
+            option = int(input("Ingresa la opcion: "))
+            match option:
+                case 1:
+                    generator_array()
+                case 2:
+                    access_algoritms()
+                case _:
+                    raise ValueError("")
         except:
             os.system("cls")
             input("!Dato no valido!")
         else:
-            pass
+            valid_date = False
         finally:
             os.system("cls")
 
--- src/algoritmos_ordenamiento/infrastructure/logic/algoritms.py
+++ src/algoritmos_ordenamiento/infrastructure/logic/algoritms.py
@@ -1,11 +1,78 @@
+import algoritmos_ordenamiento.infrastructure.data.array_default as default
+import os 
+
 def access_algoritms():
+    os.system("cls")
+    valid_date = True
+    option = 0
+    while valid_date:
+        try:
+            print("Que deseas hacer")
+            print("1 | Ejecutar el algoritmo de ordenamiento insertion_sort")
+            print("2 | Ejecutar el algoritmo de ordenamiento selection_sort")
+            print("3 | Ejecutar el algoritmo de ordenamiento quick_sort")
+            option = int(input("Ingresa la opcion: "))
+            match option:
+                case 1:
+                    insertion_sort(array_dificulty())
+                case 2:
+                    selection_sort(array_dificulty())
+                case 3:
+                    quick_sort(array_dificulty())
+                case _:
+                    input("hola???")
+                    raise ValueError("")
+        except:
+            os.system("cls")
+            print(option, type(option))
+            input("!Dato no valido!")
+        else:
+            valid_date = False
+        finally:
+            os.system("cls")   
+
+def array_dificulty():
+    os.system("cls")   
+    valid_date = True
+    while valid_date:
+        try:
+            print("Selecciona la dificulta para el algoritmo")
+            print("1 | Easy 100 elemento")
+            print("2 | Easy_medium 1000 elementos")
+            print("3 | Medium 10k elementos")
+            print("4 | Medium-hard 100k elementos")
+            print("5 | Hard 800k elementos")
+            print("6 | Personalizado")
+            option = int(input("Ingresa la opcion: "))
+            match option:
+                case 1:
+                    return default.array_100
+                case 2:
+                    return default.array_1000
+                case 3:
+                    return default.array_10k
+                case 4:
+                    return default.array_100k
+                case 5:
+                    return default.array_800k
+                case 6:
+                    pass
+                case _:
+                    raise ValueError("")
+        except:
+            os.system("cls")
+            input("!Dato no valido!")
+        else:
+            valid_date = False
+        finally:
+            os.system("cls") 
+    
+
+def insertion_sort(array): 
     pass
 
-def insertion_sort(): 
+def selection_sort(array):
     pass
 
-def selection_sort():
-    pass
-
-def quick_sort():
+def quick_sort(array):
     pass
